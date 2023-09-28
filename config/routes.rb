3490a3011e8a6541954ff1dd0d5f8fca9fa9b22f
Cedric Garcia <cgdm@tutanota.com>
2023-09-28 18:54:31 +0200

    yo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'pages/bookings', to: 'pages#bookings'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :hotels, only: %i[index show]
+  resources :rooms, only: %i[index show] do
+    resources :bookings, only: %i[create edit update]
+  end
+
+  resources :bookings, only: %i[destroy]
 
   # Defines the root path route ("/")
   # root "articles#index"
